5be2ce4b2fe98ea571218e4108118c26d22e7c2a
Phat117 <ntphat110705@gmail.com>
2026-02-09 20:40:41 +0700

    Create logger, env types

---
+++ packages/common/src/env.ts
@@ -0,0 +1,23 @@
+import { ZodObject, ZodRawShape, z } from "zod";
+interface EnvOptions {
+  source?: NodeJS.ProcessEnv;
+  serviceName?:string;
+}
+
+type SchemaOutput<T extends ZodRawShape> = ZodObject<T>["_output"];
+
+export const createEnv = <T extends ZodRawShape>(schema:T,options?:EnvOptions):SchemaOutput<T> => {
+  const {source=process.env,serviceName='service'} = options || {};
+
+  const parsed = z.object(schema).safeParse(source);
+
+  if(!parsed.success){
+    const formatedErrors = parsed.error.format();
+    throw new Error(
+      `[${serviceName}] Environment variables validation failed: ${JSON.stringify(formatedErrors,null,2)}`
+    )
+  }
+  return parsed.data;
+}
+
+export type EnvSchema<T extends ZodRawShape> = ZodObject<T>;
---
+++ packages/common/src/index.ts
@@ -0,0 +1,4 @@
+export type { Logger } from 'pino';
+export * from './env';
+export * from './logger';
+
