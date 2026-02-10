58ff9244ac2f8ea776073a0d5e0633912ed81b6d
Phat117 <ntphat110705@gmail.com>
2026-02-10 22:19:36 +0700

    Refresh Token Model

--- services/auth-service/src/models/index.ts
+++ services/auth-service/src/models/index.ts
@@ -1,14 +1,13 @@
 import { sequelize } from "../db/sequelize";
 import { logger } from "../utils/logger";
+import RefreshToken from "./refresh-token.model";
+
 import UserCredentials from "./user-credentials.model";
-import type {
-  UserCredentialsAttributes,
-  UserCredentialsCreationAttributes,
-} from "./user-credentials.model";
 
 export const initModels = async () =>{
   await sequelize.sync();
   logger.info("Models synchronized");
 }
 
-export { UserCredentials, UserCredentialsAttributes, UserCredentialsCreationAttributes };
+export { RefreshToken, UserCredentials };
+
